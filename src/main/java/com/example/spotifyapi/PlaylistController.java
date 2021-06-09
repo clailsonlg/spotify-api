dbd93cf2202074af37054a7acb6f0c5ff11cf294
Clailson Lucas <clailsonlucas@gmail.com>
2021-06-08 21:55:16 -0300

    Heroku publication, fixing CrossOrigin to heroku spotify-angular frontend

--- src/main/java/com/example/spotifyapi/PlaylistController.java
+++ src/main/java/com/example/spotifyapi/PlaylistController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://spotify-frontend-angular.herokuapp.com")
 @RestController
 @RequestMapping("/spotify")
 public class PlaylistController {
